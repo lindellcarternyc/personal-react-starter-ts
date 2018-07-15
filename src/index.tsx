168dc91da63fa8be9fde4c3c6781b670121123eb
Lindell Carter <lindellcarternyc@gmail.com>
2018-07-15 19:20:01 -0400

    add styles

--- src/index.tsx
+++ src/index.tsx
@@ -5,4 +5,8 @@ import ReactDOM from 'react-dom'
 // COMPONENTS
 import App from './components/App'
 
+
+// STYLES
+import './styles/app.scss'
+
 ReactDOM.render(<App />, document.getElementById('app'))
