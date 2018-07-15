9d25baf286a8560b8ef9a008770e638efeeaddaf
Lindell Carter <lindellcarternyc@gmail.com>
2018-07-15 18:16:58 -0400

    app root html and render

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+// PACKAGE DEPENDENCIES
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+
+// COMPONENTS
+import App from './components/App'
+
+ReactDOM.render(<App />, document.getElementById('app'))
